8bfb5d07576ab29f09f6a1e58a548a2c4d949cc8
Bernard Banta <banta.bernard@gmail.com>
2014-05-08 15:25:17 +0300

    margin-bottom 40px

--- vendor/assets/javascripts/wait-dialog.js
+++ vendor/assets/javascripts/wait-dialog.js
@@ -1,6 +1,6 @@
 var waitDialog;
 waitDialog = waitDialog || (function () {
-    var pleaseWaitDiv = $('<div class="modal hide" id="pleaseWaitDialog" data-backdrop="static" data-keyboard="false"><div class="modal-body"><div style="margin-top: 35px;"><span id="wait_dialog_text" style="text-align: center;"></span></div><div class="progress progress-striped active PushDown_40"><div class="bar" style="width: 100%;"></div></div></div></div>');
+    var pleaseWaitDiv = $('<div class="modal hide" id="pleaseWaitDialog" data-backdrop="static" data-keyboard="false"><div class="modal-body"><div style="margin-top: 35px;"><span id="wait_dialog_text" style="text-align: center;"></span></div><div class="progress progress-striped active" style="margin-bottom: 40px;"><div class="bar" style="width: 100%;"></div></div></div></div>');
     return {
         showPleaseWait: function() {
             pleaseWaitDiv.modal();
